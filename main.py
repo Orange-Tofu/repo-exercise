abaa65883c64baba77607ee8f2b82db90eec8d48
Akash Sil <108827497+Orange-Tofu@users.noreply.github.com>
2023-12-16 23:19:36 +0530

    Done and Dusted

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import check_internet
 import rMQR_generator
 import meme_gen
 import git_scheduler
+import time
 
 #Global Variables
 LOCATION = "M:\\CS\\TitTat\\TechnoSeek-V2.0"
@@ -15,11 +16,14 @@ def main():
 
     if (internet_status == True):
         iterations = int(input("Enter the number of iterations:"))
+        sleep_timer = int(input("Enter the break amount between every commit (in secs):"))
 
         for i in range(0, iterations):
+            time.sleep(sleep_timer)
             data = meme_gen.get_data()  #To get data for QR code
             rMQR_generator.Generator(data, LOCATION)    #To generate QR code
             git_scheduler.git_all(LOCATION, FILE_NAME, COMMIT_MSG)  #To commit 
+            print(f"\nIteration {i+1} done!! \n\n")
 
     else:
         #Call later
