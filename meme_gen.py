a8c416ce0e4b48c659e674fd0b0da15f84456973
Akash Sil <108827497+Orange-Tofu@users.noreply.github.com>
2024-03-17 14:20:25 +0530

    Update meme_gen.py

--- meme_gen.py
+++ meme_gen.py
@@ -2,9 +2,9 @@ import random
 
 content = [
     "https://youtu.be/dQw4w9WgXcQ?si=XatZH3LX2xPvymIh", #Rick Roll
-    "https://youtu.be/YLF4sKkUzSQ?si=_hjIoHX62C4S_sbx", #Dilon Ka Shotter
-    "https://youtu.be/i1ojUmdF42U?si=NBvGQxKho-qZF2rP", #Emotional Damage
-    #Add more here
+ "https://youtu.be/i1ojUmdF42U?si=NBvGQxKho-qZF2rP", #Emotional Damage
+    
+#Add more links to memes here in the given format
 ]
 
 def get_data():
